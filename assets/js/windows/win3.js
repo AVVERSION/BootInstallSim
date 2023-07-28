db7a0fe36442350d2fb4229970cdcca16b203d1c
AVVERSION <79741360+AVVERSION@users.noreply.github.com>
2023-07-28 20:54:27 +0530

    Added boot loading screen pt1

---
+++ assets/js/windows/win3.js
@@ -0,0 +1,6 @@
+document.addEventListener("keyDown", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 123){
+        // TODO: Write Code here for what happens when F12 is pressed.
+    }
+  });
