feb09b9d358960262381cd6c68ea7a7308c3b73f
AVVERSION <79741360+AVVERSION@users.noreply.github.com>
2023-07-31 21:09:48 +0530

    Fixed Minor Bugs showing white bg in win2.html

--- assets/js/windows/win3.js
+++ assets/js/windows/win3.js
@@ -1,6 +1,10 @@
-document.addEventListener("keyDown", function(event) {
-    event.preventDefault();
+document.addEventListener("keydown", function(event) {
     if (event.keyCode == 123){
+        event.preventDefault();
         // TODO: Write Code here for what happens when F12 is pressed.
+        document.body.style.backgroundColor = "#180153";
+        document.querySelector("#wininfo").classList.add("hide");
+        document.querySelector("#winlogo").classList.add("hide");
+        document.querySelector("#loader").classList.add("hide");
     }
   });
